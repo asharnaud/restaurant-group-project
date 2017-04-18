406894fa5cacd7e4ef7c505f28fd78af186aa0b4
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-04-18 10:48:08 -0500

    Issue 12 - added the latests news API

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var $ = window.jQuery
+
+// The function to get the location for the Google map.
 function initMap () {
   var newOrleans = {lat: 30.0688, lng: -89.930881}
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -5,7 +8,16 @@ function initMap () {
     center: newOrleans
     })
   var marker = new google.maps.Marker({
-      position: newOrleans,
-      map: map
+    position: newOrleans,
+    map: map
   })
 }
+
+// This retrieves the news api and replaces the html of the news section with what is retrieved.
+$.get('https://json-data.herokuapp.com/restaurant/news/1', function (data) {
+  $('#title').html(data.title + '  ' + data.date_published)
+  $('.latest-news p').html(data.post)
+})
+.fail(function () {
+  $('.latest-news p').html('Oh no! Sorry, looks like something went wrong on our end.')
+})
