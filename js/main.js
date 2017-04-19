d8de74de47eb62a53cfcd09bbcd7c4f301eeee3c
Pablo Jurado <pjurado85@gmail.com>
2017-04-19 12:06:20 -0500

    finish cleaning up render menu functions

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function initMap () {
 }
 
 // This retrieves the news api data and replaces the html of the news section with what is retrieved.
-function dataToEl (data) {
+function dataToNews (data) {
   $('#title').html(data.title + '  ' + data.date_published)
   $('#news').html(data.post)
 }
@@ -26,7 +26,7 @@ function responseFail (el) {
 responseFail($('#news'))
 
 var urlNews = 'https://json-data.herokuapp.com/restaurant/news/1'
-$.get(urlNews, dataToEl).done(dataToEl).fail(responseFail)
+$.get(urlNews).done(dataToNews).fail(responseFail)
 
 // This retrieves the daily special api data and replaces the html of the today's special section with what is retrieved.
 function dataToSpecial (data) {
@@ -37,32 +37,36 @@ function dataToSpecial (data) {
 responseFail($('#dailySpecial'))
 
 var urlDailySpecial = 'https://json-data.herokuapp.com/restaurant/special/1'
-$.get(urlDailySpecial, dataToSpecial).done(dataToSpecial).fail(responseFail)
+$.get(urlDailySpecial).done(dataToSpecial).fail(responseFail)
 
-// This retrieves the menu api data
+// This is the API call for the restaurant menu
 var urlMenu = 'https://json-data.herokuapp.com/restaurant/menu/1'
-$.get(urlMenu, renderMenu).done(dataToSpecial).fail(responseFail)
+$.get(urlMenu).done(renderMenu).fail(responseFail)
 
+// loops through the object and gets name and properties
 function renderMenu (data) {
-  $('#menu').append('<h3>Appetizers</h3>')
-  data.appetizers.forEach(function (item) {
-    $('#menu').append(menuDataToHtml(item))
-  })
-  $('#menu').append('<h3>Entrees</h3>')
-  data.entrees.forEach(function (item) {
-    $('#menu').append(menuDataToHtml(item))
-  })
-  $('#menu').append('<h3>Sides</h3>')
-  data.sides.forEach(function (item) {
+  for (var item in data) {
+    if (data.hasOwnProperty(item)) {
+      createMenuItems(item, data[item])
+    }
+  }
+}
+// first uses the name to render the title, then loops through each element
+// on the menu and calls the function that returns the html menu elements
+function createMenuItems (name, obj) {
+  var title = '<h3>' + firstLetterToUpper(name) + '</h3>'
+  $('#menu').append(title)
+  obj.forEach(function (item) {
     $('#menu').append(menuDataToHtml(item))
   })
 }
 
+// render html tags and css classes with the menu api content
 function menuDataToHtml (food) {
   var element = ''
   element += '<h4 class="food-title">' + food.item + '</h4>'
   element += '<p class="food-description">' + food.description + '</p>'
-  element += '<span>' + food.price + '</span>'
+  element += '<span> $' + food.price + '</span>'
   element += '<div class="food-icon-wrapper">'
   element += '<i title="vegan" class="fa fa-leaf ' + checkIconStatus(food.vegan) + '">' + '</i>'
   element += '<i title="spicy" class="fa fa-thermometer-full ' + checkIconStatus(food.spicy) + '">' + '</i>'
@@ -75,4 +79,8 @@ function menuDataToHtml (food) {
 function checkIconStatus (item) {
   if (item === 1) return 'active'
   return 'inactive'
+}
+
+function firstLetterToUpper (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
